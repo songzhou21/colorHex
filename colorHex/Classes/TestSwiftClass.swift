c4c20f67065dede53a1ae449282876197b49ee6f
Song Zhou <zhousong1993@gmail.com>
2018-05-09 17:11:31 +0800

    dir changes

---
+++ colorHex/Classes/TestSwiftClass.swift
@@ -0,0 +1,14 @@
+//
+//  TestSwiftClass.swift
+//  ColorHex
+//
+//  Created by songzhou on 2018/5/9.
+//
+
+import Foundation
+
+public class TestSwiftClass {
+    public func test() {
+        print("test from swift")
+    }
+}
